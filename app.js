68cc1512947040dcf7299ad6803bc13e8fdb255e
rkophs <rkophs@hotmail.com>
2014-02-02 12:22:26 -0700

    i

--- app.js
+++ app.js
@@ -89,6 +89,10 @@ app.get('/login', function(req, res) {
     res.render('login');
 });
 
+app.get('/:file', restrict, function(req, res){
+    res.render(file);
+});
+
 app.post('/login', function(req, res) {
     authenticate(req.body.username, req.body.password, function(err, user) {
         if (user) {
