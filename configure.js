be15f4f99cfeec98c6730e3756198abd506727d6
rkophs <rkophs@hotmail.com>
2014-02-02 09:37:43 -0700

    insert

--- configure.js
+++ configure.js
@@ -1,41 +1,48 @@
 var fs = require('fs');
 var prompt = require('prompt');
+var hash = require('pass').hash;
 
 var onErr = function(err) {
     console.log(err);
     return 1;
 };
-var list_options = function() {
-    console.log("1. ssl_key <path_to_key>");
-    console.log("2. ssl_crt <path_to_crt>");
-    console.log("3. port <listening_port_#>");
-    console.log("4. useradd <user_name>");
-    console.log("5. exit");
-};
+
 var properties = [
     {
         name: 'username',
-        validator: /^[a-zA-Z\s\-]+$/,
-        warning: 'Username must be only letters, spaces, or dashes'
+        validator: /^[a-zA-Z0-9\s\-]+$/,
+        warning: 'Username must be only letters, numbers, spaces, or dashes'
     },
     {
         name: 'password',
         hidden: true
+    },
+    {
+        name: 'ssl key file (.key)'
+    },
+    {
+        name: 'ssl certificate (.crt)'
+    },
+    {
+        name: 'port',
+        validator: /^[a-zA-Z0-9\s\-]+$/,
+        warning: 'Username must be only numbers'
     }
-
 ];
 
 var configure = function() {
-    console.log("Welcome to your new IDE server.\nThe following is a list of configuration options:");
-    list_options();
-    prommpt.get(properties, function(err, result){
+    console.log("Welcome to your new IDE server.\nPress enter to skip an of the following prompts.");
+    console.log("If you skip a prompt, it's previous entry will persist if existent,\n otherwise a default will be set.");
+    prompt.get(properties, function(err, result){
         if(err){
             return onErr(err);
         }
         console.log('Command-line input received:');
-        console.log('  Username: ' + result.username);
-        console.log('  Password: ' + result.password);
+        console.log(result);
     });
+    var file_name = "conf.json";
+    var conf_obj = JSON.parse(fs.readFileSync(file_name));
+    console.log(conf_obj);
 };
 
 configure();
