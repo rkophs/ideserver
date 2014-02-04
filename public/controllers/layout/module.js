25c24201149da2b373144bec23499672f8b2948b
rkophs <rkophs@hotmail.com>
2014-02-03 17:59:46 -0700

    i

--- public/controllers/layout/module.js
+++ public/controllers/layout/module.js
@@ -1,17 +1,6 @@
 var ideApp = angular.module("ideApp", []);
 
 ideApp.controller('LayoutControl', function($scope) {
-
-    angular.element(document).ready(function() {
-        console.log("init called");
-        setTimeout(function() {
-            var e1 = ace.edit("e1");
-            e1.setTheme("ace/theme/twilight");
-            var mode = "json";
-            setMode(e1, mode);
-        }, 4000);
-    });
-
     $scope.init = function() {
 
     };
@@ -19,6 +8,12 @@ ideApp.controller('LayoutControl', function($scope) {
 }).directive('layout', function() {
     return {
         restrict: "E",
-        templateUrl: 'controllers/layout/module.html'
+        templateUrl: 'controllers/layout/module.html',
+        link: function(scope, element, attrs){
+            var e1 = ace.edit("e1");
+            e1.setTheme("ace/theme/twilight");
+            var mode = "json";
+            setMode(e1, mode);
+        }
     };
 });
