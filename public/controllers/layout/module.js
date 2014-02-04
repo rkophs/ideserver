683ae24dacfc3aff5db01368ef9244b9cd2da6d7
rkophs <rkophs@hotmail.com>
2014-02-03 21:36:08 -0700

    i

--- public/controllers/layout/module.js
+++ public/controllers/layout/module.js
@@ -32,7 +32,11 @@ ideApp.controller('LayoutControl', function($scope) {
     return {
         restrict: "E",
         templateUrl: 'controllers/layout/module.html',
-        link: function(scope, element, attrs) {
-        }
     };
-});
+}).directive('repeatDone', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last) { // all are rendered
+                scope.$eval(attrs.repeatDone);
+            }
+        }
+    });
