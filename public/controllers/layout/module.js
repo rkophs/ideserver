b01f42cf1e8252c323620d3154817a42fcbd9d89
rkophs <rkophs@hotmail.com>
2014-02-04 14:04:42 -0700

    ii

--- public/controllers/layout/module.js
+++ public/controllers/layout/module.js
@@ -72,18 +72,18 @@ ideApp.controller('LayoutControl', function($scope) {
     };
 });
 
-//var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
-//
-//  $scope.items = items;
-//  $scope.selected = {
-//    item: $scope.items[0]
-//  };
-//
-//  $scope.ok = function () {
-//    $modalInstance.close($scope.selected.item);
-//  };
-//
-//  $scope.cancel = function () {
-//    $modalInstance.dismiss('cancel');
-//  };
-//};
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
