2e5369edb254c04a7dbdb3597112058dd1b4bdfa
rkophs <rkophs@hotmail.com>
2014-02-03 21:50:42 -0700

    i

--- public/controllers/layout/module.js
+++ public/controllers/layout/module.js
@@ -27,11 +27,11 @@ ideApp.controller('LayoutControl', function($scope) {
         restrict: "E",
         templateUrl: 'controllers/editor/module.html',
         link: function(scope, element, attrs){
-            scope.data.e = element.data("id");
-            var e = ace.edit(scope.data.e);
-            e.setTheme("ace/theme/twilight");
-            var mode = "json";
-            setMode(e, mode);
+//            scope.data.e = element.data("id");
+//            var e = ace.edit(scope.data.e);
+//            e.setTheme("ace/theme/twilight");
+//            var mode = "json";
+//            setMode(e, mode);
         }
     };
 })
