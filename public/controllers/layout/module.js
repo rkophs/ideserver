415a3b585bed671e732b01db0e308f06f858bf4f
rkophs <rkophs@hotmail.com>
2014-02-03 17:47:02 -0700

    i

--- public/controllers/layout/module.js
+++ public/controllers/layout/module.js
@@ -1,11 +1,6 @@
 var ideApp = angular.module("ideApp", []);
 
 ideApp.controller('LayoutControl', function($scope) {
-    var e1 = ace.edit("e1");
-    e1.setTheme("ace/theme/twilight");
-    var mode = "json";
-    setMode(e1, mode);
-
     $scope.init = function() {
         console.log("init called");
         var e1 = ace.edit("e1");
